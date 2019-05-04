11ae418c339fc66baccdb47e876ed933891dabc5
Spencer Haney <SpencasaurusRex@gmail.com>
2019-05-03 23:04:25 -0400

    Initial commit

---
+++ Assets/Scripts/AirportClickHandler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class AirportClickHandler
+{
+    readonly GameData gameData;
+
+    public AirportClickHandler(GameData data)
+    {
+        gameData = data;
+    }
+
+    public void Update()
+    {
+        if (!Input.GetMouseButtonDown(0)) return;
+        
+        RaycastHit2D hit = Physics2D.GetRayIntersection(Camera.main.ScreenPointToRay(Input.mousePosition));
+        if (hit.transform == null) return;
+
+        for (int i = 0; i < gameData.Airports.Count; i++)
+        {
+            if (hit.transform != gameData.Airports[i].transform) continue;
+            var connections = gameData.AirportData[i].Connections;
+            Debug.Log(i + " which is connected to " + string.Join(", ", connections));
+        }
+    }
+}
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Level : ScriptableObject
+{
+    public AirportData[] Airports;
+}
+
+[Serializable]
+public class AirportData
+{
+    public int CurrentNumber;
+    public int TargetNumber;
+    public float X;
+    public float Y;
+
+    public List<int> Connections = new List<int>();
+
+    [HideInInspector]
+    public int Delta;
+    [HideInInspector]
+    public bool Dirty;
+}
